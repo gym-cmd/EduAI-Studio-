e38550761b4bdb3e0adad4ecce13e9a22dcb3a8d
Jorn Nys <jorn.nys@mylighthouse.com>
2026-03-16 21:47:10 +0100

    small fixes

--- src/learning_agent/agent_engine_app.py
+++ src/learning_agent/agent_engine_app.py
@@ -5,13 +5,11 @@ from vertexai.agent_engines import AdkApp
 
 from .agent import root_agent
 
-if False:  # Whether or not to use Express Mode
-    vertexai.init(api_key=os.environ.get("GOOGLE_API_KEY"))
-else:
-    vertexai.init(
-        project=os.environ.get("GOOGLE_CLOUD_PROJECT"),
-        location=os.environ.get("GOOGLE_CLOUD_LOCATION"),
-    )
+
+vertexai.init(
+    project=os.environ.get("GOOGLE_CLOUD_PROJECT"),
+    location=os.environ.get("GOOGLE_CLOUD_LOCATION"),
+)
 
 adk_app = AdkApp(
     agent=root_agent,
